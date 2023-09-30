aa27ee27331728851f31a2443c094d16382817d3
divyesh2123 <divyesh2198@gmail.com>
2023-09-30 19:11:44 +0530

    Add project files.

---
+++ Controllers/DisplayInfoController.cs
@@ -0,0 +1,24 @@
+﻿using DBFirstProject.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBFirstProject.Controllers
+{
+    public class DisplayInfoController : Controller
+    {
+
+        public EmployeeDbContext employeeDbContext { get; set; }    
+        public DisplayInfoController(EmployeeDbContext _employeeDbContext)
+        {
+            employeeDbContext = _employeeDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Employee> employees = new List<Employee>();
+            var d = employeeDbContext.Employees.ToList();
+
+
+                return View(employees);
+        }
+    }
+}
